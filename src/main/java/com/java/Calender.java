ac60bc114265361607775dea7d1388e83de4fba1
Apps16 <aparna9493@gmail.com>
2017-08-12 11:51:44 +0530

    Change-Id: Ia5990a5489e49a3678bd57ce4031f90c4b497a34 Signed-off-by: Apps16 <aparna9493@gmail.com>

--- src/main/java/com/java/Calender.java
+++ src/main/java/com/java/Calender.java
@@ -4,7 +4,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
                                                                                                                     
-public class Calender {
+public class Calender {                 
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
                                                                                                                                                                                                                                                                                                                                                                                
