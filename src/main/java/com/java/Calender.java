0b843f1172c90ebebd0f5f6665674a4034db239c
Apps16 <aparna9493@gmail.com>
2017-08-12 10:51:22 +0530

    Added new code
    Change-Id: Idd5a085bbe579772c2949afcf46da0ea974446f3
    Signed-off-by: Apps16 <aparna9493@gmail.com>

---
+++ src/main/java/com/java/Calender.java
@@ -0,0 +1,28 @@
+package com.java;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Calender {
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+
+		String date = "0000-00-00";
+		try {
+			if (date != null) {
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+				sdf.setLenient(false);
+				Date date1 = sdf.parse(date);
+				sdf.format(date1);
+
+				System.out.println("valid");
+			} else {
+				System.out.println("valid");
+			}
+		} catch (ParseException ex) {
+			System.out.println("invalid");
+		}
+
+	}
+}
